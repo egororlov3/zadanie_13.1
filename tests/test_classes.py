629222b9f0e6a226b3c6911aca233720d262a23c
Egor Orlov <erop.orl@gmail.com>
2024-05-20 22:12:11 +0300

    new methods

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -4,38 +4,40 @@ from classes import Product
 
 
 @pytest.fixture
-def goods(products1,products2):
+def goods(product1,product2):
     return Category("sneakers", "new",
-                    [products1, products2])
+                    [product1, product2])
 
 
-def test_init_category(goods, products2, products1):
+def test_init_category(goods, product2, product1):
     assert goods.name == "sneakers"
     assert goods.description == "new"
-    assert goods.products == [products1, products2]
+    assert goods.products == [product1, product2]
     assert goods.total_categories == 2
     assert goods.total_uniques == 1
 
 
 @pytest.fixture
-def products1():
+def product1():
     return Product("New Balance", "9060 Truffle", 20000, 5)
 
 
-def test_init_products1(products1):
-    assert products1.name == "New Balance"
-    assert products1.description == "9060 Truffle"
-    assert products1.price == 20000
-    assert products1.quantity == 5
+def test_init_products1(product1):
+    assert product1.name == "New Balance"
+    assert product1.description == "9060 Truffle"
+    assert product1.price == 20000
+    assert product1.quantity == 5
 
 
 @pytest.fixture
-def products2():
+def product2():
     return Product("Nike", "Airmax 95 Black", 10000, 10)
 
 
-def test_init_products2(products2):
-    assert products2.name == "Nike"
-    assert products2.description == "Airmax 95 Black"
-    assert products2.price == 10000
-    assert products2.quantity == 10
+def test_init_products2(product2):
+    assert product2.name == "Nike"
+    assert product2.description == "Airmax 95 Black"
+    assert product2.price == 10000
+    assert product2.quantity == 10
+
+
